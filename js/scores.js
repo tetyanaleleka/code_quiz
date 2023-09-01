530b6d449769082c0fd9a0461bd7a3ba999527e8
tetyanaleleka <tana.leleka@gmail.com>
2023-09-01 18:55:43 +0100

    Separated js files are added

---
+++ js/scores.js
@@ -0,0 +1,18 @@
+function printHighScores(){
+
+}
+
+function clearHighScore(){
+
+}
+
+
+let clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", clearHighScore);
+
+
+// document.getElementById("clear").onClick = clearHighScores;
+
+printHighScores();
+
+
